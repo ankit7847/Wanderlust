bc134be3fd6902fa53c7fd97bf54be929d4dc994
Ankit Rathore <ankitrathore7847@gmail.com>
2024-08-31 21:09:44 +0530

    models

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const Listing = require("../models/listing.js");
 app.get("/",(req,res)=>{
     res.send("Hi,i am root");
 });
