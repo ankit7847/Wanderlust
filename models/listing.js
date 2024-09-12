fc116439a011b5d6f3bb1b655bcf2441d7052ac7
Ankit Rathore <ankitrathore7847@gmail.com>
2024-09-12 21:51:50 +0530

    data.js and index,js creare

--- models/listing.js
+++ models/listing.js
@@ -2,19 +2,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title : {
-        type:String,
-        require : true,
-    },
-    description:true,
-    image:{
-        type :String,
-        set:(v)=> v === "" ? "https://www.istockphoto.com/photo/sunset-beach-scene-with-tall-palm-trees-people-walking-and-a-coastal-building-under-gm1922043323-555427353?utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fsunsset-with-coconut-tree&utm_medium=affiliate&utm_source=unsplash&utm_term=sunsset+with+coconut+tree%3A%3A%3A" : v,
-    },
-    price:Number,
-    location:String,
-    country:String,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
-const Listing = mongoose.model("Listing",listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
