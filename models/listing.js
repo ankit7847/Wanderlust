86fb25a12c73a2aae43b8cb380a9a8ab6a048f7f
Ankit Rathore <ankitrathore7847@gmail.com>
2024-09-19 15:25:23 +0530

    Creating models for reviews and review format

--- models/listing.js
+++ models/listing.js
@@ -1,3 +1,4 @@
+const { ref } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -15,7 +16,15 @@ const listingSchema = new Schema({
   price: Number,
   location: String,
   country: String,
+  reviews : [
+    {
+      type : Schema.Types.ObjectId,
+      ref : "Review"
+    },
+  ],
 });
+
+
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
