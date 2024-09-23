ab576b3b2f815093c57d6e2af433a1709e3c1d0d
Ankit Rathore <ankitrathore7847@gmail.com>
2024-09-23 17:16:43 +0530

    adding cookies

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require('express-session');
 
 const listings = require("./router/listing.js");
 const reviews = require("./router/review.js");
@@ -30,6 +31,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"public")));
+
+const sessionOptions = {
+  secret : "mysecretsession",
+  resave : true,
+  saveUninitialized: true,
+  cookie:{
+    expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge : 7 * 24 * 60 * 60 * 1000,
+    httpOnly : true,
+  },
+};
+
+app.use(session(sessionOptions));
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
