e23547f2563c59e47404e8ee43070f83548fea02
Ankit Rathore <ankitrathore7847@gmail.com>
2024-09-26 22:15:12 +0530

    adding cloud store

--- router/listing.js
+++ router/listing.js
@@ -6,6 +6,8 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedIn,isOwner,validateListing} = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 
 router
