f7a7951670fcfd7a3bf510e8c09e69fa9d9186ed
Ankit Rathore <ankitrathore7847@gmail.com>
2024-09-26 22:47:35 +0530

    cloudconfig

--- controllers/listings.js
+++ controllers/listings.js
@@ -23,8 +23,11 @@ module.exports.index = async (req, res) => {
   };
 
   module.exports.createListing = async (req, res, next) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url,filename};
     await newListing.save();
     req.flash("success" ,"New Listing Add");
     res.redirect("/listings");
--- router/listing.js
+++ router/listing.js
@@ -6,14 +6,15 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedIn,isOwner,validateListing} = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
 
 
 router
 .route("/")
 .get( wrapAsync(listingController.index))
-.post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+.post(isLoggedIn,upload.single("listing[image]"), validateListing, wrapAsync(listingController.createListing));
 
 // New Route
 router.get("/new",isLoggedIn, listingController.renderNewForm);
