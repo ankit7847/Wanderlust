408894474cfe091acb91c3550286b6b0010346fc
Ankit Rathore <ankitrathore7847@gmail.com>
2024-09-26 17:45:11 +0530

    re route the paths

--- router/user.js
+++ router/user.js
@@ -5,13 +5,15 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/user.js")
-router.get("/signup", userController.renderSignupForm);
 
-router.post("/signup", wrapAsync(userController.signup));
+router.route("/signup")
+.get( userController.renderSignupForm)
+.post( wrapAsync(userController.signup))
 
-router.get("/login", userController.renderLoginForm);
-
-router.post("/login", saveRedirectUrl, passport.authenticate("local", {
+router.route("/login")
+.get(userController.renderLoginForm)
+.post(
+     saveRedirectUrl, passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true
 }), userController.login);
